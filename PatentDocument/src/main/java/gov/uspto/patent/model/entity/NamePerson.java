3cae217263e133ba468ea70ccc83915b73aa9d72
Brian Feldman <brian.feldman@uspto.gov>
2016-11-09 12:57:08 -0500

    Fix getting Person Name Abbreviation

--- PatentDocument/src/main/java/gov/uspto/patent/model/entity/NamePerson.java
+++ PatentDocument/src/main/java/gov/uspto/patent/model/entity/NamePerson.java
@@ -3,6 +3,7 @@ package gov.uspto.patent.model.entity;
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 
+import gov.uspto.common.text.StringCaseUtil;
 import gov.uspto.patent.InvalidDataException;
 
 /*
@@ -24,89 +25,109 @@ import gov.uspto.patent.InvalidDataException;
 */
 public class NamePerson extends Name {
 
-	private String prefix; // Title: Mr., Mrs., Dr.
-	private final String firstName;
-	private final String middleName;
-	private final String lastName;
+    private String prefix; // Title: Mr., Mrs., Dr.
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
 
-	public NamePerson(String firstName, String lastName) throws InvalidDataException {
-		this(firstName, null, lastName);
-	}
+    public NamePerson(String firstName, String lastName) throws InvalidDataException {
+        this(firstName, null, lastName);
+    }
 
-	public NamePerson(String firstName, String middleName, String lastName) throws InvalidDataException {
-		super(buildFullName(firstName, middleName, lastName));
+    public NamePerson(String firstName, String middleName, String lastName) throws InvalidDataException {
+        super(buildFullName(firstName, middleName, lastName));
 
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
 
-		validate();
-	}
+        validate();
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public boolean validate() throws InvalidDataException {
-		String fullName = super.getName();
+    public boolean validate() throws InvalidDataException {
+        String fullName = super.getName();
 
-		if (Strings.isNullOrEmpty(lastName)) {
-			throw new InvalidDataException("Invalid PersonName, lastname can not be blank");
-		}
+        if (Strings.isNullOrEmpty(lastName)) {
+            throw new InvalidDataException("Invalid PersonName, lastname can not be blank");
+        }
 
-		if (Strings.isNullOrEmpty(fullName) || fullName.length() < 2) {
-			throw new InvalidDataException("Invalid PersonName, fullName can not be blank or smaller than 2.");
-		}
+        if (Strings.isNullOrEmpty(fullName) || fullName.length() < 2) {
+            throw new InvalidDataException("Invalid PersonName, fullName can not be blank or smaller than 2.");
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Get abbreviated version of name.
-	 * 
-	 * @return Last Name, first initial.
-	 */
-	public String getAbbreviatedName() {
-		if (Strings.isNullOrEmpty(firstName)) {
-			return lastName + ", " + firstName.substring(0, 1) + '.';
-		} else {
-			return lastName;
-		}
-	}
+    /**
+     * Fix case for Name part when in all capitals.
+     * 
+     * @param part
+     * @return
+     */
+    private String normalizeCase(String part) {
+        if (part.matches("^[A-Z]+$")) {
+            return StringCaseUtil.toTitleCase(part);
+        } else {
+            return part;
+        }
+    }
 
-	public String getMiddleName() {
-		return middleName;
-	}
+    /**
+     * Get abbreviated version of name, with normalized case.
+     * 
+     * @return Last Name, first initial.
+     */
+    public String getAbbreviatedName() {
+        if (!Strings.isNullOrEmpty(firstName)) {
+            StringBuilder stb = new StringBuilder();
+            stb.append(normalizeCase(lastName));
+            stb.append(", ");
+            stb.append(firstName.substring(0, 1).toUpperCase());
+            stb.append(".");
 
-	public String getPrefix() {
-		return prefix;
-	}
+            return stb.toString();
+        } else {
+            return normalizeCase(lastName);
+        }
+    }
 
-	public void setPrefix(String prefix) {
-		this.prefix = prefix;
-	}
+    public String getMiddleName() {
+        return middleName;
+    }
 
-	private static String buildFullName(String firstName, String middleName, String lastName) {
-		StringBuilder sb = new StringBuilder();
-		if (!Strings.isNullOrEmpty(lastName)) {
-			sb.append(lastName).append(", ");
-		}
+    public String getPrefix() {
+        return prefix;
+    }
 
-		if (!Strings.isNullOrEmpty(firstName)) {
-			sb.append(Joiner.on(" ").skipNulls().join(firstName, middleName));
-		}
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
 
-		return sb.toString().trim();
-	}
+    private static String buildFullName(String firstName, String middleName, String lastName) {
+        StringBuilder sb = new StringBuilder();
+        if (!Strings.isNullOrEmpty(lastName)) {
+            sb.append(lastName).append(", ");
+        }
 
-	@Override
-	public String toString() {
-		return "PersonName[prefix=" + prefix + ", firstName=" + firstName + ", middleName=" + middleName + ", lastName="
-				+ lastName + ", fullName=" + super.getName() + ", suffix=" + super.getSuffix() + ", synonym="
-				+ super.getSynonyms() + "]";
-	}
+        if (!Strings.isNullOrEmpty(firstName)) {
+            sb.append(Joiner.on(" ").skipNulls().join(firstName, middleName));
+        }
+
+        return sb.toString().trim();
+    }
+
+    @Override
+    public String toString() {
+        return "PersonName[prefix=" + prefix + ", firstName=" + firstName + ", middleName=" + middleName + ", lastName="
+                + lastName + ", fullName=" + super.getName() + ", suffix=" + super.getSuffix() + ", synonym="
+                + super.getSynonyms() + "]";
+    }
 }
---
+++ PatentDocument/src/test/java/gov/uspto/patent/model/entity/NamePersonTest.java
@@ -0,0 +1,44 @@
+package gov.uspto.patent.model.entity;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import gov.uspto.patent.InvalidDataException;
+
+public class NamePersonTest {
+
+    @Test(expected = InvalidDataException.class)
+    public void invalidNameNullLastName() throws InvalidDataException {
+        Name name = new NamePerson("John", null);
+    }
+
+    @Test(expected = InvalidDataException.class)
+    public void invalidNameEmptyLastName() throws InvalidDataException {
+        Name name = new NamePerson("John", "");
+    }
+
+    @Test
+    public void testAbbreviatedName() throws InvalidDataException {
+        NamePerson name = new NamePerson("John", "DOE");
+        String abbrev = name.getAbbreviatedName();
+        String expect = "Doe, J.";
+        assertEquals(expect, abbrev);
+    }
+
+    @Test
+    public void testAbbreviatedName2() throws InvalidDataException {
+        NamePerson name = new NamePerson("John", "McMillian");
+        String abbrev = name.getAbbreviatedName();
+        String expect = "McMillian, J.";
+        assertEquals(expect, abbrev);
+    }
+
+    @Test
+    public void testFullNameTitleCase() throws InvalidDataException {
+        NamePerson name = new NamePerson("JOHN", "DOE");
+        String titleCase = name.getNameTitleCase();
+        String expect = "Doe, John";
+        assertEquals(expect, titleCase);
+    }
+}
