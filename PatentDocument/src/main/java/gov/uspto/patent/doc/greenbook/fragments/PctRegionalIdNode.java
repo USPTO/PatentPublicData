845e4217817a2b5364d76efb82f1bdb5ce8c1a51
Brian Feldman <brian.feldman@uspto.gov>
2017-01-09 14:59:18 -0500

    Fixed NPE.

--- PatentDocument/src/main/java/gov/uspto/patent/doc/greenbook/fragments/PctRegionalIdNode.java
+++ PatentDocument/src/main/java/gov/uspto/patent/doc/greenbook/fragments/PctRegionalIdNode.java
@@ -17,83 +17,107 @@ import gov.uspto.patent.model.DocumentDate;
 import gov.uspto.patent.model.DocumentId;
 
 public class PctRegionalIdNode extends DOMFragmentReader<List<DocumentId>> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PctRegionalIdNode.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(PctRegionalIdNode.class);
 
-    private static final String FRAGMENT_PATH = "/DOCUMENT/PCTA";
+	private static final String FRAGMENT_PATH = "/DOCUMENT/PCTA";
 
-    public PctRegionalIdNode(Document document) {
-        super(document);
-    }
+	public PctRegionalIdNode(Document document) {
+		super(document);
+	}
 
-    @Override
-    public List<DocumentId> read() {
-        List<DocumentId> docIds = new ArrayList<DocumentId>();
+	@Override
+	public List<DocumentId> read() {
+		List<DocumentId> docIds = new ArrayList<DocumentId>();
 
-        Node pctGroupN = document.selectSingleNode(FRAGMENT_PATH);
-        if (pctGroupN == null) {
-            return docIds;
-        }
+		Node pctGroupN = document.selectSingleNode(FRAGMENT_PATH);
+		if (pctGroupN == null) {
+			return docIds;
+		}
 
-        Node pctFilingIdN = pctGroupN.selectSingleNode("PCN");
-        if (pctFilingIdN != null) {
-            DocumentId filindDocId = buildDocId(pctFilingIdN.getText());
+		DocumentId pctFilingId = filingId(pctGroupN);
+		if (pctFilingId != null){
+			docIds.add(pctFilingId);
+		}
 
-            Node pctFilingDateN = pctGroupN.selectSingleNode("PD3"); // filing date.
-            if (pctFilingDateN != null) {
-                String filingDateStr = pctFilingDateN.getText().replaceAll("[^0-9]", "");
-                try {
-                    DocumentDate filingDate = new DocumentDate(filingDateStr);
-                    filindDocId.setDate(filingDate);
-                } catch (InvalidDataException e) {
-                    LOGGER.warn("Invalid PCT Filing Date: {}", filingDateStr);
-                }
-            }
-            docIds.add(filindDocId);
-        }
+		DocumentId pctPubId = filingId(pctGroupN);
+		if (pctPubId != null){
+			docIds.add(pctPubId);
+		}
 
-        Node pctPubIdN = pctGroupN.selectSingleNode("PCP");
-        if (pctPubIdN != null) {
-            DocumentId pubDocId = buildDocId(pctPubIdN.getText());
+		return docIds;
+	}
 
-            Node pctPubDateN = pctGroupN.selectSingleNode("PCD"); // publication date
-            if (pctPubDateN != null) {
-                String pubDateStr = pctPubDateN.getText().replaceAll("[^0-9]", "");
-                try {
-                    DocumentDate pubDate = new DocumentDate(pubDateStr);
-                    pubDocId.setDate(pubDate);
-                } catch (InvalidDataException e) {
-                    LOGGER.warn("Invalid PCT Publication Date: {}", pubDateStr);
-                }
-            }
-            docIds.add(pubDocId);
-        }
+	public DocumentId publicationId(Node pctGroupN){
+		Node pctPubIdN = pctGroupN.selectSingleNode("PCP");
+		if (pctPubIdN != null) {
+			DocumentId pubDocId = buildDocId(pctPubIdN.getText());
 
-        return docIds;
-    }
+			if (pubDocId != null) {
+				Node pctPubDateN = pctGroupN.selectSingleNode("PCD");
+				if (pctPubDateN != null) {
+					String pubDateStr = pctPubDateN.getText().replaceAll("[^0-9]", "");
+					try {
+						DocumentDate pubDate = new DocumentDate(pubDateStr);
+						pubDocId.setDate(pubDate);
+					} catch (InvalidDataException e) {
+						LOGGER.warn("Invalid PCT Publication Date: {}", pubDateStr);
+					}
+				}
 
-    private static final Pattern PCT_ID_PATTERN = Pattern.compile("^(?:PCT/)?([A-Z]{2})([0-9]{2}/[0-9]{4,})$");
+				return pubDocId;
+			}
+		}
 
-    private DocumentId buildDocId(String pctDocIdString) {
-        Matcher matcher = PCT_ID_PATTERN.matcher(pctDocIdString);
+		return null;
+	}
 
-        if (matcher.matches()) {
-            String countryCodeStr = matcher.group(1);
-            String docId = matcher.group(2).replaceAll("/", "");
+	public DocumentId filingId(Node pctGroupN){
+		Node pctFilingIdN = pctGroupN.selectSingleNode("PCN");
+		if (pctFilingIdN != null) {
+			DocumentId filindDocId = buildDocId(pctFilingIdN.getText());
 
-            CountryCode countryCode = CountryCode.UNKNOWN;
+			if (filindDocId != null) {
+				Node pctFilingDateN = pctGroupN.selectSingleNode("PD3");
+				if (pctFilingDateN != null) {
+					String filingDateStr = pctFilingDateN.getText().replaceAll("[^0-9]", "");
+					try {
+						DocumentDate filingDate = new DocumentDate(filingDateStr);
+						filindDocId.setDate(filingDate);
+					} catch (InvalidDataException e) {
+						LOGGER.warn("Invalid PCT Filing Date: {}", filingDateStr);
+					}
+				}
 
-            try {
-                countryCode = CountryCode.fromString(countryCodeStr);
-            } catch (InvalidDataException e) {
-                LOGGER.warn("Failed to lookup CountryCode: {}", countryCodeStr);
-            }
+				return filindDocId;
+			}
+		}
 
-            return new DocumentId(countryCode, docId);
-        }
+		return null;
+	}
+	
+	private static final Pattern PCT_ID_PATTERN = Pattern.compile("^(?:PCT/)?([A-Z]{2})([0-9]{2}/[0-9]{4,})$");
 
-        LOGGER.warn("PCT DocID did not pattern: {}", pctDocIdString);
+	private DocumentId buildDocId(String pctDocIdString) {
+		Matcher matcher = PCT_ID_PATTERN.matcher(pctDocIdString);
 
-        return null;
-    }
+		if (matcher.matches()) {
+			String countryCodeStr = matcher.group(1);
+			String docId = matcher.group(2).replaceAll("/", "");
+
+			CountryCode countryCode = CountryCode.UNKNOWN;
+
+			try {
+				countryCode = CountryCode.fromString(countryCodeStr);
+			} catch (InvalidDataException e) {
+				LOGGER.warn("Failed to lookup CountryCode: {}", countryCodeStr);
+			}
+
+			return new DocumentId(countryCode, docId);
+		}
+
+		LOGGER.warn("PCT DocID did not pattern: {}", pctDocIdString);
+
+		return null;
+	}
 
 }
