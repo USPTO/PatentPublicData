8a9757c68336a4aa510d041c0a9c18be4b68a502
Brian Feldman <bgfeldm@yahoo.com>
2018-02-22 09:03:19 -0500

    Update download.sh

--- resources/scripts/download.sh
+++ resources/scripts/download.sh
@@ -13,11 +13,12 @@ CLASSPATH="${PROJECTPATH}/lib/*:${PROJECTPATH}/lib/dependency-jars/*"
 JAVA="java -cp ${CLASSPATH} -Dlog4j.configuration=file:${PROJECTPATH}/conf/log4j.properties"
 
 #
-# type: [application, grant]    requited;  patent document type
-# years     required; comma for individual years; dash for year range
+# type: [application, grant, gazette]    required;  patent document type
+# date     required; 20140101-20161231
 # limit     download limit
 # skip      skip over limit
+# async     Async Downloads
 # filename  specific bulk file name to download
 # outdir    directory to download to.
 #
-${JAVA} gov.uspto.bulkdata.cli2.BulkData --source google --type application --limit=${LIMIT} --outdir="${PROJECTPATH}/download"
+${JAVA} gov.uspto.bulkdata.cli2.BulkData --type application --limit=${LIMIT} --outdir="${PROJECTPATH}/download"
