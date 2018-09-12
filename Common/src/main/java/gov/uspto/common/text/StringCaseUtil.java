246b8565392f85e3a0a5a0fe3b8860d2700bb7c7
Brian Feldman <bfeldman@uspto.gov>
2018-09-12 11:12:08 -0400

    Improvements normalizing Title's case to title case

--- Common/src/main/java/gov/uspto/common/text/StringCaseUtil.java
+++ Common/src/main/java/gov/uspto/common/text/StringCaseUtil.java
@@ -1,14 +1,32 @@
 package gov.uspto.common.text;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.google.common.base.Joiner;
 
 public class StringCaseUtil {
 
-	private static List<String> lowerCaseWords = Arrays.asList(new String[] { "A", "An", "And", "As", "At", "But", "By",
-			"For", "In", "Nor", "Of", "Off", "On", "Or", "Per", "So", "The", "To", "Up", "Via", "With", "Yet" });
+	private static Set<String> lowerCaseWords = new HashSet<String>();
+	private static Set<String> prefixes = new HashSet<String>();
+	private static Set<String> lowerTrailHyphen = new HashSet<String>();
+	private static Set<String> prepositions = new HashSet<String>();
+
+	static {
+		prepositions.addAll(Arrays.asList(new String[] { "As", "At", "But", "By", "For", "From", "In", "Into", "Of", "Off", "On", "Onto", "To", "Up",
+				"Via", "With", "Within", "Without", "Like" }));
+
+		lowerCaseWords.addAll(Arrays.asList(new String[] { "The", "A", "An", "And", "Nor", "Or", "Per", "So", "Yet", "Into" }));
+		lowerCaseWords.addAll(prepositions);
+
+		prefixes.addAll(Arrays.asList(new String[] {"Re", "Pre", "Post", "Anti", "Fore", "De", "Non", "Dis", "En", "Em", "In", "Im", "Il", "Ir", "Inter",
+				"Mid", "Mis", "Over", "Sub", "Super", "Supra", "Trans", "Un", "Under", "Co", "Pro", "Proto", "Para", "Meta", "Ex", "Semi", "Ultra", "Neo",
+				"Mini",	"Micro", "Mega", "Macro", "Intra", "Infra", "Hypo", "Hyper", "Bi", "Tri", "Di", "Multi", "Auto", "Bio", "Geo", "Hydro", "Extra",
+				"Pseudo", "Socio", "Electro", "Single", "Dual", "Triple", "Quad"}));
+
+		lowerTrailHyphen.addAll(Arrays.asList(new String[] { "Like", "Type", "Driven" }));
+	}
 
 	public static String join(String delimiter, String... elements) {
 		// Similar to Java 8's : "String.join(delimiter, elements);"
@@ -26,34 +44,143 @@ public class StringCaseUtil {
 		return sb.toString();
 	}
 
+	/**
+	 * Title Case
+	 *
+	 *<pre>
+	 * Patent Titles originate in one of three forms: all uppercase, sentence case or proper title case. 
+	 * Here a best effort is performed to normalized to Title Case.
+	 * 
+	 * The Style Rules used loosely follows the Chicago Simple and Wikipedia Style Manuals; with a preference
+	 * to lowercase words when possible but only if it can be done with a light weight manner 
+	 * (without a NLP parser) and receives a decent level of accuracy.
+	 *
+	 * 1) Maintain Uppercase if detect the use of lowercase and word is not in lowerCaseWords (articles, ...)
+	 * 2) Quotes or brackets: maintain case of words within
+	 * 3) First and Last Word: capitalize
+	 * 4) articles (the,a,an): lowercase
+	 * 5) coordinating conjunctions (and, but, for, or, nor): lowercase
+	 * 6) short prepositions : lowercase
+	 * 7) common prepositions: lowercase
+	 * 8) Hyphens split into individual words and use rules above.
+	 * 9) Hyphenated lead prefix: treat prefix and word as if a single word; capitalize prefix and lowercase trailing word.
+	 * 10) Hyphenated trailing word with verb tense suffixes ['ing','ed']: capitalize first word then lowercase trailing word.
+	 * 11) Slashes: treat each word as a noun or verb, capitalize each word
+	 *</pre>
+	 *
+	 * @param text
+	 * @return
+	 */
 	public static String toTitleCase(String text) {
 		if (text == null) {
 			return null;
 		}
 
+		/*
+		 * If text is not all capitals then maintain capitals
+		 */
+		boolean maintainCapitals = false;
+		if (text.matches(".*[a-z]{3,}.*")) { // sequence of 3 at least lowercase chars
+			maintainCapitals = true;
+		}
+
 		String[] words = text.split("\\s");
 
+		boolean inBlock = false;
+		char wantCloseBlockChar = '?';
 		for (int i = 0; i < words.length; i++) {
+
+			/*
+			 * Block Quoted or Bracketed; maintain original lettercase.
+			 */
+			char firstChar = words[i].charAt(0);
+			char lastChar = words[i].charAt(words[i].length()-1);
+			if (('‘' == firstChar && '’' == lastChar) || '(' == firstChar && ')' == lastChar || (')' == lastChar && !inBlock)) {
+				if ('‘' == firstChar) {
+					words[i] = "'" + words[i].substring(1, words[i].length()-1) + "'";
+				}
+				continue;
+			}
+			else if ('‘' == firstChar || '(' == firstChar || words[i].indexOf('(') != -1) {
+				if ('‘' == firstChar) {
+					words[i] = "'" + words[i].substring(1);
+					wantCloseBlockChar = '’';
+				} else {
+					wantCloseBlockChar = ')';
+				}
+				inBlock = true;
+				continue;
+			}
+			else if (wantCloseBlockChar == lastChar) {
+				if ('’' == lastChar) {
+					words[i] = words[i].substring(0, words[i].length()-1) + "'";
+				}
+				inBlock = false;
+				continue;
+			}
+			else if (inBlock || maintainCapitals && Character.isUpperCase(firstChar) && !lowerCaseWords.contains(words[i])) {
+				words[i] = normHyphenWord(words[i]);
+				words[i] = normSlashWord(words[i]);
+				continue;
+			}
+
 			words[i] = capitalizeFirstLetter(words[i]);
+			words[i] = normHyphenWord(words[i]);
+			words[i] = normSlashWord(words[i]);
 
-			String[] hypenwords = words[i].split("-");
-			if (hypenwords.length > 1) {
-				hypenwords = capitalizeFirstLetter(hypenwords);
-				words[i] = Joiner.on("-").join(hypenwords);
-			}
-
-			String[] slashWords = words[i].split("/");
-			if (slashWords.length > 1) {
-				slashWords = capitalizeFirstLetter(slashWords);
-				words[i] = Joiner.on("/").join(slashWords);
-			}
-
-			if (i > 0 && lowerCaseWords.contains(words[i])) {
+			/*
+			 * Lowercase Always Words
+			 */
+			if ((i > 0 || i == words.length-1) && lowerCaseWords.contains(words[i])) {
 				words[i] = words[i].toLowerCase();
 			}
 		}
 
 		return (Joiner.on(" ").join(words));
+	}
+
+	
+	
+	/**
+	 * Fix Hyphen Word
+	 * 
+	 * @param word
+	 * @return
+	 */
+	private static String normHyphenWord(String word) {
+		String ret = word;
+		String[] hypenwords = word.split("[-\\u2013\\u2014]");
+		if (hypenwords.length > 1) {
+			for (int j = 0; j < hypenwords.length; j++) {
+				hypenwords[j] = capitalizeFirstLetter(hypenwords[j]);
+				if (j != 0 && lowerCaseWords.contains(hypenwords[j])) {
+					hypenwords[j] = hypenwords[j].toLowerCase();
+				}
+			}
+
+			if (hypenwords[0].length() == 1 || 
+					prefixes.contains(hypenwords[0]) || 
+					hypenwords[1].endsWith("ing") || 
+					hypenwords[1].endsWith("ed") ||
+					// maybe also "en"
+					lowerTrailHyphen.contains(hypenwords[1])) {
+				
+						hypenwords[1] = hypenwords[1].toLowerCase();
+			}
+
+			ret = Joiner.on("-").join(hypenwords);
+		}
+		return ret;
+	}
+
+	private static String normSlashWord(String word) {
+		String ret = word;
+		String[] slashWords = word.split("/");
+		if (slashWords.length > 1) {
+			slashWords = capitalizeFirstLetter(slashWords);
+			ret = Joiner.on("/").join(slashWords);
+		}
+		return ret;
 	}
 
 	public static String[] capitalizeFirstLetter(String[] strArr) {
--- Common/src/test/java/gov/uspto/common/text/StringCaseTest.java
+++ Common/src/test/java/gov/uspto/common/text/StringCaseTest.java
@@ -12,13 +12,78 @@ import gov.uspto.common.text.StringCaseUtil;
 
 public class StringCaseTest {
 
+	// meta-analysis, anti-inflammatory, neo-orthodox, de-emphasize, re-enact, pre-election
+	
 	private static Map<String, String> TitleValidFromTo = new LinkedHashMap<String, String>();
 	static {
-		TitleValidFromTo.put("THE PHONE WITH BOOK", "The Phone with Book");
+		TitleValidFromTo.put("LED Lamp", "LED Lamp");
+		TitleValidFromTo.put("Light emitting diode lamp", "Light Emitting Diode Lamp");
+		TitleValidFromTo.put("LIGHT-EMITTING DIODE CIRCLE LAMP", "Light-emitting Diode Circle Lamp");
+		TitleValidFromTo.put("PRICE-AFFIXING MACHINE", "Price-affixing Machine");
+		TitleValidFromTo.put("IN-LINE MIXER", "In-line Mixer");
+		TitleValidFromTo.put("Dual-Tip Marker", "Dual-tip Marker");
+		TitleValidFromTo.put("LIRIOPE MUSCARI PLANT NAMED ‘LIRF’", "Liriope Muscari Plant Named 'LIRF'");
+		TitleValidFromTo.put("CHESTNUT PLANT NAMED ‘AU BUCK IV’", "Chestnut Plant Named 'AU BUCK IV'");
+		TitleValidFromTo.put("IN-WHEEL MOTOR WITH HIGH DURABILITY", "In-wheel Motor with High Durability");
+		TitleValidFromTo.put("WIND-POWERED PNEUMATIC ENGINE AND A MOTOR VEHICLE EQUIPPED WITH THE ENGINE",
+				"Wind-powered Pneumatic Engine and a Motor Vehicle Equipped with the Engine");
+		TitleValidFromTo.put("MAGNETIC TILT AND RAISE/LOWER MECHANISMS FOR A VENETIAN BLIND", 
+				"Magnetic Tilt and Raise/Lower Mechanisms for a Venetian Blind");
+		TitleValidFromTo.put("DOUBLE OVER-DOOR HOOK", "Double Over-door Hook");
+		TitleValidFromTo.put("SQUEEZE-AND-TURN CHILD RESISTANT CLOSURE", "Squeeze-and-Turn Child Resistant Closure");	
+		TitleValidFromTo.put("Heating/cooling System for Indwelling Heat Exchange Catheter", "Heating/Cooling System for Indwelling Heat Exchange Catheter");
+		TitleValidFromTo.put("Method and Apparatus for Satellite Positioning of Earth-Moving Equipment", 
+				"Method and Apparatus for Satellite Positioning of Earth-moving Equipment");
+		TitleValidFromTo.put("Attachment for Harvesting Stalk-Like Goods Where Each Cutting and Conveying Element Is Controlled Individually", 
+				"Attachment for Harvesting Stalk-like Goods Where Each Cutting and Conveying Element Is Controlled Individually");		
+		TitleValidFromTo.put("Method for Controlling the Feeding of a Web Substrate Into a Printing Press", 
+				"Method for Controlling the Feeding of a Web Substrate into a Printing Press");
+		TitleValidFromTo.put("Straddle-Type Vehicle", "Straddle-type Vehicle");
+		TitleValidFromTo.put("Pressure-sensitive poly(N-vinyl lactam) adhesive composition and method for producing and using same", 
+				"Pressure-Sensitive poly(N-vinyl lactam) Adhesive Composition and Method for Producing and Using Same");
+		TitleValidFromTo.put("Triple-blade Ice Skating Footwear and Associated Method", "Triple-blade Ice Skating Footwear and Associated Method");
+		TitleValidFromTo.put("RIDE-ON TURF MOWING MACHINE ROLLOVER PROTECTION ASSEMBLY", 
+				"Ride-on Turf Mowing Machine Rollover Protection Assembly");
+		TitleValidFromTo.put("Method and apparatus to increase the contrast ratio of the image produced by a LCoS based light engine", 
+				"Method and Apparatus to Increase the Contrast Ratio of the Image Produced by a LCoS Based Light Engine");
+		TitleValidFromTo.put("Method and apparatus for early diagnosis of Alzheimer's using non-invasive eye tomography by terahertz", 
+				"Method and Apparatus for Early Diagnosis of Alzheimer's Using Non-invasive Eye Tomography by Terahertz");
+		TitleValidFromTo.put("LED backlight device with deviated LED pitch", "LED Backlight Device with Deviated LED Pitch");
+		TitleValidFromTo.put("Light-emitting module and light-emitting system", "Light-emitting Module and Light-emitting System");
+		TitleValidFromTo.put("Emergency under-lighting systems for vehicles", "Emergency Under-lighting Systems for Vehicles");		
+		TitleValidFromTo.put("Blender/food processor blade arrangement for small throated blender jars",
+				"Blender/Food Processor Blade Arrangement for Small Throated Blender Jars");
+		TitleValidFromTo.put("Liquid crystal display and backlight module thereof", "Liquid Crystal Display and Backlight Module Thereof");
+		TitleValidFromTo.put("Ceiling mount for x-ray system", "Ceiling Mount for X-ray System");
+		TitleValidFromTo.put("Method and apparatus for multi-directional fiber optic connection", 
+				"Method and Apparatus for Multi-directional Fiber Optic Connection");
+
+		
+		
+		// Motor-Driven Power Steering Unit Support Structure
+		// Hand-Driven Wheelchair
+
 		TitleValidFromTo.put("FOR THE PRESEDENT OF THE CLUB", "For the Presedent of the Club");
 		TitleValidFromTo.put("PRESENT FOR TALKS", "Present for Talks");
-		TitleValidFromTo.put("THE COMPUTER/LAPTOP FOR THE WORK GROUP", "The Computer/Laptop for the Work Group");
-		TitleValidFromTo.put("THE COMPUTER-LAPTOP FOR THE WORK GROUP", "The Computer-Laptop for the Work Group");
+		TitleValidFromTo.put("THE COMPUTER/LAPTOP FOR THE WORK GROUP", "The Computer/Laptop for the Work Group"); // slash
+		TitleValidFromTo.put("CATCH ON FIRE DURING A HOT RE-ENTRY", "Catch on Fire During a Hot Re-entry"); // hyphen prefix repeat letter
+		TitleValidFromTo.put("ATHLETES WHO RE-SIGN WITH THEIR TEAMS", "Athletes Who Re-sign with Their Teams"); // hyphen prefix no repeat letter
+		TitleValidFromTo.put("COMPUTER E-MAIL SYSTEM", "Computer E-mail System"); // hyphen prefix first is not a word
+		TitleValidFromTo.put("OVER-THE-COUNTER DRUGS", "Over-the-Counter Drugs");
+		TitleValidFromTo.put("UP-TO-DATE SYSTEM", "Up-to-Date System");
+		TitleValidFromTo.put("MID-SEPTEMBER", "Mid-september");
+		TitleValidFromTo.put("DE-EMPHASIZE", "De-emphasize");
+		TitleValidFromTo.put("RE-COVER", "Re-cover");
+		TitleValidFromTo.put("CO-OCCUR", "Co-occur");
+		TitleValidFromTo.put("SINGLE-CORE", "Single-core");
+		TitleValidFromTo.put("MULTI-CORE", "Multi-core");
+		TitleValidFromTo.put("DUAL-CORE", "Dual-core");
+		TitleValidFromTo.put("TRIPLE-CORE", "Triple-core");
+		TitleValidFromTo.put("QUAD-CORE", "Quad-core");
+		TitleValidFromTo.put("CO\u2013OCCUR", "Co-occur"); // unicode \u2014 is en-dash which is transformed to dash.
+		TitleValidFromTo.put("CO\u2014OCCUR", "Co-occur"); // unicode \u2014 is em-dash which is transformed to dash.
+		TitleValidFromTo.put("LONG-TERM EFFECTS", "Long-Term Effects");
+		TitleValidFromTo.put("WHY SUNLESS TANNING IS A HOT TREND", "Why Sunless Tanning Is a Hot Trend"); // short verb "is" should be uppercase.
 	}
 
 	@Test
