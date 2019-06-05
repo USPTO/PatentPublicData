4ea06bb55947fe06b32e609b4cb8ecfae918c0ac
Brian Feldman <bfeldman@uspto.gov>
2019-06-05 14:04:45 -0400

    Update scripts which call transform

--- resources/scripts/transform.bat
+++ resources/scripts/transform.bat
@@ -6,18 +6,37 @@ IF [%1]==[] GOTO NO_ARGUMENT
 REM ProjectPath is one directory up from script bin directory.
 SET PROJECTPATH=%~dp0..
 
+REM SET JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_72\jre\bin\java
+
 SET FILE=%1
 SET OUTPUTDIR=%PROJECTPATH%\download\transformed
 
 REM  OUTPUT (raw, object, json, json_flat, text)
 IF [%2]==[] (SET OUTPUT="json") else (SET OUTPUT="%2")
 
+IF "%JAVA_HOME%" == "" GOTO JVM_NOT_FOUND
 IF NOT EXIST %FILE% (SET FILE="%PROJECTPATH%/download/%FILE%")
 IF NOT EXIST %FILE% GOTO FILE_NOT_FOUND
 
 SET CLASSPATH="%PROJECTPATH%\lib\*;%PROJECTPATH%\lib\dependency-jars\*;"
 
-SET JAVA=java -cp %CLASSPATH% -Dlog4j.configuration="file:%PROJECTPATH%\conf\log4j.properties"
+SET GC_TUNE=-XX:NewRatio=3 ^
+   -XX:SurvivorRatio=4 ^
+   -XX:TargetSurvivorRatio=90 ^
+   -XX:MaxTenuringThreshold=8 ^
+   -XX:+UseConcMarkSweepGC ^
+   -XX:+UseParNewGC ^
+   -XX:ConcGCThreads=4 -XX:ParallelGCThreads=4 ^
+   -XX:+CMSScavengeBeforeRemark ^
+   -XX:PretenureSizeThreshold=64m ^
+   -XX:+UseCMSInitiatingOccupancyOnly ^
+   -XX:CMSInitiatingOccupancyFraction=50 ^
+   -XX:CMSMaxAbortablePrecleanTime=6000 ^
+   -XX:+CMSParallelRemarkEnabled ^
+   -XX:+ParallelRefProcEnabled ^
+   -XX:-OmitStackTraceInFastThrow
+
+SET JAVA=%JAVA_HOME% -server -Xms1G -Xmx2G %GC_TUNE% -cp %CLASSPATH% -Dlog4j.configuration="file:%PROJECTPATH%\conf\log4j.properties"
 
 IF NOT EXIST %OUTPUTDIR% (md %OUTPUTDIR%)
 
@@ -25,8 +44,12 @@ IF NOT EXIST %OUTPUTDIR% (md %OUTPUTDIR%)
 
 GOTO DONE
 
+:JVM_NOT_FOUND
+echo ERROR: JAVA_HOME not set
+exit /B
+
 :FILE_NOT_FOUND
-echo File Not Found
+echo ERROR: File Not Found
 exit /B
 
 :NO_ARGUMENT
--- resources/scripts/transform.sh
+++ resources/scripts/transform.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+####### Install Java 8 or above.
+##
+#### UBUNTU
+## apt-cache search java | grep openjdk
+## sudo apt install default-jre
+## sudo apt install openjdk-12-jre-headless
+## sudo apt install openjdk-11-jre-headless
+## sudo apt install openjdk-10-jre-headless
+## sudo apt install openjdk-9-jre-headless
+## sudo apt install openjdk-8-jre-headless
+## JAVA_HOME=/usr/java/default
+## JAVA_HOME=/usr/lib/jvm/java-12-openjdk-amd64
+## JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+## JAVA_HOME=/usr/lib/jvm/java-10-openjdk-amd64
+## JAVA_HOME=/usr/lib/jvm/java-9-openjdk-amd64
+## JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+## JAVA_HOME=/usr/lib/jvm/java-8-oracle
+## JAVA_HOME=/usr/lib/jvm/java-12-oracle
+##
+##### REDHAT / FEDORA
+## yum search java | grep openjdk
+## sudo yum install java-1.8.0-openjdk-headless
+## sudo yum install java-11-openjdk-headless.x86_64
+## JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+## sudo rpm -ivh jre-8u73-linux-x64.rpm
+## JAVA_HOME=/usr/java
+
+####### Set JAVA_HOME to match installed java
+JAVA_HOME=/usr
+
 USAGE(){
     echo "Usage: ./`basename $0` <FILE|DIRECTORY>"
     echo "Usage: ./`basename $0` <FILE|DIRECTORY> <OUTPUT>"
@@ -9,6 +39,11 @@ USAGE(){
 if [ ! "$#" -ge "1" ]; then
     USAGE
     exit 1
+fi
+
+if [ -z ${JAVA_HOME} ]; then
+	echo "JAVA_HOME not set"
+	exit 1
 fi
 
 ## ProjectPath is one directory up from script bin directory.
@@ -40,7 +75,23 @@ fi
 
 CLASSPATH="${PROJECTPATH}/lib/*:${PROJECTPATH}/lib/dependency-jars/*"
 
-JAVA="java -Xms1G -Xmx1G -cp ${CLASSPATH} -Dlog4j.configuration=file:${PROJECTPATH}/conf/log4j.properties"
+GC_TUNE=('-XX:NewRatio=3' \
+        '-XX:SurvivorRatio=4' \
+        '-XX:TargetSurvivorRatio=90' \
+        '-XX:MaxTenuringThreshold=8' \
+        '-XX:+UseConcMarkSweepGC' \
+        '-XX:+UseParNewGC' \
+        '-XX:ConcGCThreads=4' '-XX:ParallelGCThreads=4' \
+        '-XX:+CMSScavengeBeforeRemark' \
+        '-XX:PretenureSizeThreshold=64m' \
+        '-XX:+UseCMSInitiatingOccupancyOnly' \
+        '-XX:CMSInitiatingOccupancyFraction=50' \
+        '-XX:CMSMaxAbortablePrecleanTime=6000' \
+        '-XX:+CMSParallelRemarkEnabled' \
+        '-XX:+ParallelRefProcEnabled' \
+        '-XX:-OmitStackTraceInFastThrow')
+
+JAVA="${JAVA_HOME}/bin/java -server -Xms1G -Xmx2G ${GC_TUNE} -cp ${CLASSPATH} -Dlog4j.configuration=file:${PROJECTPATH}/conf/log4j.properties"
 
 ${JAVA} gov.uspto.bulkdata.cli.Transformer -f="${FILE}" --type="${OUTPUT}" --outDir="${OUTPUTDIR}" --outBulk=true --prettyPrint=false
 
