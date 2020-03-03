8edab49d92a7f79e7fea1672dec74669c3fa2555
Brian Feldman <bfeldman@uspto.gov>
2020-03-03 16:20:59 -0500

    Greenbook : ClassificationSearch normalizing USPC classification

--- PatentDocument/src/main/java/gov/uspto/patent/doc/greenbook/fragments/ClassificationSearchNode.java
+++ PatentDocument/src/main/java/gov/uspto/patent/doc/greenbook/fragments/ClassificationSearchNode.java
@@ -66,12 +66,30 @@ public class ClassificationSearchNode extends DOMFragmentReader<Set<PatentClassi
 		List<Node> classNodes = CLASSXP.selectNodes(document);
 		for (Node classN : classNodes) {
 			Node fssN =  CLASSUBXP.selectSingleNode(classN);
+
 			String searchClass = classN.getText().trim();
+			searchClass = Strings.padStart(searchClass, 3, '0');
+
 			String[] fss = fssN.getText().trim().split(";");
 			for(String subClass: fss) {
-				searchClass = Strings.padStart(searchClass, 3, '0');
-				subClass = Strings.padStart(subClass, 3, '0');
-				String searchCL = searchClass + subClass;
+
+				// 06315-15.8 -> 063015-015.8
+				String subClass2 = Strings.padStart(subClass, 3, '0');
+				String[] parts = subClass.split("-");
+				if (parts.length == 2) {
+					parts[0] = Strings.padStart(parts[0], 3, '0');
+					if (parts[1].contains(".")) {
+						String[] part2P = parts[1].split("\\.");
+						part2P[0] = Strings.padStart(part2P[0], 3, '0');
+						parts[1] = String.join(".", part2P);
+					} else {
+						parts[1] = Strings.padStart(parts[1], 3, '0');
+					}
+					subClass2 = String.join("-", parts);
+				}
+				LOGGER.debug("Normalized Classification: {} -> {}", searchClass+subClass, searchClass+subClass2);
+
+				String searchCL = searchClass + subClass2;
 				UspcClassification uspc = new UspcClassification(searchCL.replace(" ", ""), false);
 				try {
 					uspc.parseText(searchCL);
